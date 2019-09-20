be4278c19978656055680dce93425f2ddbcd4866
Christian Dior Cleofas <christianc@sxi.com.ph>
2019-09-20 13:15:41 +0800

    Pushed landing page files to repo.

---
+++ js/screen-width.js
@@ -0,0 +1,13 @@
+
+var getWidth;
+var myDOM = document.getElementById("getWidth");
+var appendDOM = document.createElement("p");
+
+function getCurrentWidth() {
+    getWidth = window.innerWidth;
+    appendDOM.innerHTML = getWidth.toString();
+    myDOM.appendChild(appendDOM);
+}
+
+window.addEventListener("resize", getCurrentWidth)
+getCurrentWidth();
